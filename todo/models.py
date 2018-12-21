56bd0e796767bf77aeab2c0dbbc5c420ebf8b142
Abhishek Sah <abhi.sah.97@gmail.com>
2018-12-21 17:40:12 +0530

    [Abhishek] Adds ToDo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    message = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    time = models.DateTimeField()
